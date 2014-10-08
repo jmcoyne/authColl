4913783da7af3969f30dae242b14d1e6c87998cf
jmcoyne <jcoyne@mzinga.com>
2014-10-08 09:37:40 -0400

    Initial Commit

---
+++ authColl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  authColl
+//
+//  Created by Joan Coyne on 10/8/14.
+//  Copyright (c) 2014 Mzinga. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
